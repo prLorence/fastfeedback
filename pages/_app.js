cab5be222a217c21691c9215ba5200f17148f6bc
prLorence <paulorencereyes@gmail.com>
2022-03-28 18:49:11 +0800

    setup: chakra-ui and firestore

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
-import { ProvideAuth } from '../lib/auth';
-import '../styles/globals.css';
+import { AuthProvider } from '@/lib/auth';
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from '../styles/theme';
+// import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ProvideAuth>
-      <Component {...pageProps} />
-    </ProvideAuth>
+    <ChakraProvider theme={theme}>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ChakraProvider>
   );
 }
 
